aebe209d7ae589de2ca3fd74b78f9ac86bfae993
VeroCin <veronica_mircea@yahoo.com>
2024-12-28 17:56:24 +0200

    Update hangmanWord.js

--- hangmanWord.js
+++ hangmanWord.js
@@ -9,32 +9,37 @@ function updateDisplay() {
 function checkLetter() {
     const input = document.getElementById('guessInput').value.toLowerCase();
     document.getElementById('guessInput').value = "";
+
     if (input && input.length === 1 && /^[a-z]$/.test(input)) {
-        if (word.includes(input)) {
-            let found = false;
-            for (let i = 0; i < word.length; i++) {
-                if (word[i] === input && guessedWord[i] === '_') {
-                    guessedWord[i] = input; 
-                    found = true;
-                }
-            }
-            if (found) {
-                document.getElementById('gameResult').innerText = "Letter found!";
-            }
-        } else {
-            --lives;
-            document.getElementById('gameResult').innerText = `Lives left: ${lives} "${input}" is incorrect.`;
-        }
+        verifyLetter(input);
+        checkGameState();
+    }
+}
 
-        updateDisplay();
-
-        if (lives === 0) {
-            document.getElementById('gameResult').innerText = `Game over! The word was: "${word}"`;
-            document.getElementById('guessInput').disabled = true;
-        } else if (!guessedWord.includes('_')) {
-            document.getElementById('gameResult').innerText = "win!";
-            document.getElementById('guessInput').disabled = true;
+function verifyLetter(input) {
+    if (word.includes(input)) {
+        let found = false;
+        for (let i = 0; i < word.length; ++i) {
+            if (word[i] === input && guessedWord[i] === '_') {
+                guessedWord[i] = input;
+                found = true;
+            }
         }
+        document.getElementById('gameResult').innerText = found ? "Letter found!" : "";
+    } else {
+        --lives;
+        document.getElementById('gameResult').innerText = `Lives left: ${lives} "${input}" is incorrect.`;
+    }
+    updateDisplay();
+}
+
+function checkGameState() {
+    if (lives === 0) {
+        document.getElementById('gameResult').innerText = `Game over! The word was: "${word}"`;
+        document.getElementById('guessInput').disabled = true;
+    } else if (!guessedWord.includes('_')) {
+        document.getElementById('gameResult').innerText = "You win!";
+        document.getElementById('guessInput').disabled = true;
     }
 }
 
